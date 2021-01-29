af5a2d954fa4470b240335da3d8733a0878ca127
Csoda26 <charlie.clavel@lapiscine.pro>
2021-01-29 12:27:25 +0100

    user modif

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -17,6 +17,22 @@ class User
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -28,27 +44,10 @@ class User
      */
     private $firstname;
 
-
-
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="datetime")
      */
-    private $mail;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $update_password;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $date_update_password;
+    private $birthdate;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -56,33 +55,107 @@ class User
     private $adress;
 
     /**
-     * @ORM\Column(type="string", length=15)
+     * @ORM\Column(type="string", length=255)
      */
     private $zipcode;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="string", length=255)
      */
     private $city;
 
     /**
-     * @ORM\Column(type="string", length=50)
+     * @ORM\Column(type="string", length=255)
      */
     private $country;
 
     /**
-     * @ORM\Column(type="string", length=20)
+     * @ORM\Column(type="string", length=255)
      */
     private $phone;
 
     /**
      * @ORM\Column(type="datetime")
      */
-    private $birthdate;
+
+    private $date_update_passwword;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getName(): ?string
@@ -109,50 +182,14 @@ class User
         return $this;
     }
 
-    public function getMail(): ?string
+    public function getBirthdate(): ?\DateTimeInterface
     {
-        return $this->mail;
+        return $this->birthdate;
     }
 
-    public function setMail(string $mail): self
+    public function setBirthdate(\DateTimeInterface $birthdate): self
     {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getUpdatePassword(): ?string
-    {
-        return $this->update_password;
-    }
-
-    public function setUpdatePassword(string $update_password): self
-    {
-        $this->update_password = $update_password;
-
-        return $this;
-    }
-
-    public function getDateUpdatePassword(): ?string
-    {
-        return $this->date_update_password;
-    }
-
-    public function setDateUpdatePassword(string $date_update_password): self
-    {
-        $this->date_update_password = $date_update_password;
+        $this->birthdate = $birthdate;
 
         return $this;
     }
@@ -217,14 +254,14 @@ class User
         return $this;
     }
 
-    public function getBirthdate(): ?\DateTimeInterface
+    public function getDateUpdatePasswword(): ?\DateTimeInterface
     {
-        return $this->birthdate;
+        return $this->date_update_passwword;
     }
 
-    public function setBirthdate(\DateTimeInterface $birthdate): self
+    public function setDateUpdatePasswword(\DateTimeInterface $date_update_passwword): self
     {
-        $this->birthdate = $birthdate;
+        $this->date_update_passwword = $date_update_passwword;
 
         return $this;
     }
