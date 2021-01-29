f1695e9866c297fbe871b8f93e3f6906001746e2
Mastrakas <71260092+Mastrakas@users.noreply.github.com>
2021-01-29 15:04:32 +0100

    oui

--- migrations/Version20210129140314.php
+++ migrations/Version20210129140314.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210129135504 extends AbstractMigration
+final class Version20210129140314 extends AbstractMigration
 {
     public function getDescription() : string
     {
