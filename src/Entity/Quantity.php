753825cb8d51de71311d9386e8daa99a90fd20ce
Mastrakas <71260092+Mastrakas@users.noreply.github.com>
2021-02-01 14:58:44 +0100

    quantity

---
+++ src/Entity/Quantity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuantityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuantityRepository::class)
+ */
+class Quantity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?int
+    {
+        return $this->name;
+    }
+
+    public function setName(int $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
